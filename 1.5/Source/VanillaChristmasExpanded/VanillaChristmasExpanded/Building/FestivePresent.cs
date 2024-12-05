f63b44a07c8d997b245c6bc2779918b21d2a696c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-12-05 23:30:28 +0100

    Graphics applied

--- 1.5/Source/VanillaChristmasExpanded/VanillaChristmasExpanded/Building/FestivePresent.cs
+++ 1.5/Source/VanillaChristmasExpanded/VanillaChristmasExpanded/Building/FestivePresent.cs
@@ -6,14 +6,68 @@ using Verse;
 
 using Verse.Noise;
 using UnityEngine.Diagnostics;
+using System;
+using System.Linq;
 
 namespace VanillaChristmasExpanded
 {
     public class FestivePresent : Building, IOpenable
     {
 
+        public CompQuality compQuality;
+        public Graphic_Single cachedGraphic = null;
+        public PresentGraphicsByQuality cachedGraphicsExtension;
+        public string cachedGraphicPath="";
 
-   
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref this.cachedGraphicPath, "cachedGraphicPath", "", false);
+          
+
+        }
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            compQuality = this.TryGetComp<CompQuality>();
+            cachedGraphicsExtension = this.def.GetModExtension<PresentGraphicsByQuality>();
+            if (cachedGraphicsExtension != null)
+            {
+                LongEventHandler.ExecuteWhenFinished(delegate { StoreGraphics(); });
+            }
+            base.SpawnSetup(map, respawningAfterLoad);
+        }
+
+        public void StoreGraphics()
+        {
+            QualityCategory quality = compQuality.Quality;
+
+            if (cachedGraphicPath == "")
+            {
+                cachedGraphicPath = cachedGraphicsExtension.graphics.Where(x => x.quality == quality).First().texturePaths.RandomElement();
+            }
+            cachedGraphic = (Graphic_Single)GraphicDatabase.Get<Graphic_Single>(cachedGraphicPath, ShaderDatabase.Cutout,
+                     this.def.graphicData.drawSize, Color.white);
+            
+        }
+
+        protected override void DrawAt(Vector3 drawLoc, bool flip = false)
+        {
+            base.DrawAt(drawLoc, flip);
+           
+            if (cachedGraphicPath != "")
+            {
+                var vector = DrawPos + Altitudes.AltIncVect;
+               
+                    vector.y += Altitudes.AltInc;
+                    Vector3 vectorOffset = Vector3.zero;
+
+                cachedGraphic.DrawFromDef(vector + vectorOffset, Rot4.North, null);
+                
+            }
+        }
+        
 
         public float PriceByQuality(QualityCategory quality)
         {
---
+++ 1.5/Source/VanillaChristmasExpanded/VanillaChristmasExpanded/DefExtensions/PresentGraphicsByQuality.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaChristmasExpanded
+{
+
+    public class PresentGraphicsByQuality : DefModExtension
+    {
+        public List<PresentGraphics> graphics = null;
+
+    }
+
+    public class PresentGraphics
+    {
+        public List<string> texturePaths;
+        public QualityCategory quality;
+
+    }
+}
