e7c9777df97067b539e9829bb8d0ea1e1197951c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-12-09 15:34:50 +0100

    December 10th

---
+++ 1.5/Source/VanillaChristmasExpanded/VanillaChristmasExpanded/Harmony/SteadyEnvironmentEffects_MeltAmountAt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+using UnityEngine;
+
+namespace VanillaChristmasExpanded
+{
+   /* [HarmonyPatch(typeof(SteadyEnvironmentEffects), "MeltAmountAt")]
+    public static class VanillaChristmasExpanded_SteadyEnvironmentEffects_MeltAmountAt_Patch
+    {
+       
+
+        public static void Postfix(ref float __result)
+        {
+            
+                __result = 0;
+            
+        }
+    }*/
+}
