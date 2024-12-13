5afdf434e84449202814917dc4a341f8786f3687
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-12-13 12:48:27 +0100

    No more wall shit

---
+++ 1.5/Source/VanillaChristmasExpanded/VanillaChristmasExpanded/Harmony/BaseGenUtility_RandomCheapWallStuff.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+using RimWorld.BaseGen;
+
+
+
+namespace VanillaChristmasExpanded
+{
+
+    /*This Harmony Postfix tries to remove exotic materials from ruins
+*/
+    [HarmonyPatch(typeof(BaseGenUtility))]
+    [HarmonyPatch("RandomCheapWallStuff")]
+    [HarmonyPatch(new Type[] { typeof(TechLevel), typeof(bool) })]
+
+
+    public static class VanillaChristmasExpanded_BaseGenUtility_RandomCheapWallStuff_Patch
+    {
+        [HarmonyPostfix]
+        public static void RemoveMaterial(ref ThingDef __result)
+
+        {
+            if (__result != null)
+            {
+                if (__result == InternalDefOf.VCE_Gingerbread || __result == InternalDefOf.VCE_Snow)
+                {
+
+                    __result = ThingDefOf.BlocksGranite;
+                }
+            }
+
+
+
+        }
+
+    }
+
+
+}
