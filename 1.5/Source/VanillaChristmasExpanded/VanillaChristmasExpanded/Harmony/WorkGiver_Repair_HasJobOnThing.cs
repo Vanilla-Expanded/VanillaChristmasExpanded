9f39b0538920f329e5edcd2308a660c2b8cceb30
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-12-14 14:11:44 +0100

    Snow truly melts and can't be repaired

---
+++ 1.5/Source/VanillaChristmasExpanded/VanillaChristmasExpanded/Harmony/WorkGiver_Repair_HasJobOnThing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+using UnityEngine;
+
+namespace VanillaChristmasExpanded
+{
+    [HarmonyPatch(typeof(WorkGiver_Repair), "HasJobOnThing")]
+    public static class VanillaChristmasExpanded_WorkGiver_Repair_HasJobOnThing_Patch
+    {
+
+
+        public static void Postfix(ref bool __result, Thing t)
+        {
+            if (t.Stuff==InternalDefOf.VCE_Snow) { __result = false; }
+
+
+        }
+    }
+}
