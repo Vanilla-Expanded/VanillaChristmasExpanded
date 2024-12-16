2fc2fcafaffc0e0c624bd439233c779d79e90699
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-12-16 09:27:48 +0100

    Snow collecting fixes

--- 1.5/Source/VanillaChristmasExpanded/VanillaChristmasExpanded/Harmony/JobDriver_ClearSnow_MakeNewToils.cs
+++ 1.5/Source/VanillaChristmasExpanded/VanillaChristmasExpanded/Harmony/JobDriver_ClearSnow_MakeNewToils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Verse.AI;
 using RimWorld;
 using Verse;
 using HarmonyLib;
@@ -7,21 +7,22 @@ using UnityEngine.UIElements;
 
 namespace VanillaChristmasExpanded
 {
-    [HarmonyPatch(typeof(JobDriver_ClearSnow), "MakeNewToils")]
+    [HarmonyPatch(typeof(JobDriver), "Cleanup")]
     public static class VanillaChristmasExpanded_JobDriver_ClearSnow_MakeNewToils_Patch
     {
 
 
-        public static void Postfix(JobDriver_ClearSnow __instance)
+        public static void Postfix(JobDriver __instance)
         {
-          
 
-           
+            if (__instance is JobDriver_ClearSnow)
+            {
+
                 Thing thing = ThingMaker.MakeThing(InternalDefOf.VCE_Snow);
                 thing.stackCount = 4;
                 GenPlace.TryPlaceThing(thing, __instance.job.targetA.Cell, __instance.pawn.Map, ThingPlaceMode.Near);
 
-          
+            }
 
 
         }
